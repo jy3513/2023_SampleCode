b6ab09cf361238e48cd903ffa3beac46b21b8137
jy3513 <60872455+jy3513@users.noreply.github.com>
2023-05-29 04:20:18 +0900

    Add files via upload

---
+++ ReddotSystem.cpp
@@ -0,0 +1,173 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "ReddotSystem.h"
+
+void UReddotSystem::Initialize()
+{
+
+}
+
+void UReddotSystem::Start()
+{
+    LoadAsync();
+}
+
+void UReddotSystem::Release()
+{
+    m_ReddotData = nullptr;
+}
+
+// public.
+//===============================================================================================================================================================================
+// public - Content Reddot.
+
+#pragma region Get_Reddot
+bool UReddotSystem::GetReddot(const ERedDotConditions& InRedDotConditions)
+{
+    if (ERedDotConditions::None == InRedDotConditions)
+    {
+        UE_LOG(LogTemp, Warning, TEXT("InRedDotConditions is None"));
+        return false;
+    }
+
+    if (m_ReddotData == nullptr || m_ReddotData->IsValidLowLevel() == false)
+    {
+        UE_LOG(LogTemp, Warning, TEXT("m_ReddotData is nullptr"));
+        return false;
+    }
+
+    // get data.
+    TArray<int64> ReddotList;
+    return GetReddotInfo(InContentEnum, ReddotList);
+}
+
+bool UReddotSystem::GetReddotInfo(const ERedDotConditions& InContentEnum, TArray<int64>& OutReddotList)
+{
+    if (InContentEnum == ERedDotConditions::None)
+    {
+        UE_LOG(LogTemp, Warning, TEXT("InContentEnum is None"));
+        return false;
+    }
+
+    if (m_ReddotData == nullptr || m_ReddotData->IsValidLowLevel() == false)
+    {
+        UE_LOG(LogTemp, Warning, TEXT("UReddotSystemData is nullptr"));
+        return false;
+    }
+    
+    OutReddotList.Empty();
+    
+    for (const FReddotNotiInfo& Contents : m_ReddotData->ContentReddotList)
+    {
+        if (InContentEnum != Contents.RedDotConditions)
+        {
+            continue;
+        }
+
+        OutReddotList.Emplace(Contents.TargetIndex);
+    }
+
+    return (bool)(OutReddotList.Num() > 0);
+}
+
+#pragma endregion
+
+#pragma region Set_Reddot
+bool UReddotSystem::SetReddot(const ERedDotConditions& InRedDotConditions, const int64& InTargetIndex)
+{
+    if (InRedDotConditions == ERedDotConditions::None)
+    {
+        UE_LOG(LogTemp, Warning, TEXT("InRedDotConditions is None"));
+        return false;
+    }
+    
+    if (m_ReddotData == nullptr || m_ReddotData->IsValidLowLevel() == false)
+    {
+        UE_LOG(LogTemp, Warning, TEXT("UReddotSystemData is nullptr"));
+        return false;
+    }
+
+    // check.
+    for (const FReddotNotiInfo& Contents : m_ReddotData->ContentReddotList)
+    {
+        if (Contents.RedDotConditions != InRedDotConditions ||
+            Contents.TargetIndex != InTargetIndex)
+        {
+            continue;
+        }
+
+        // 동일한게 있음.
+        return false;
+    }
+
+    // emplace.
+    m_ReddotData->ContentReddotList.Emplace(FReddotNotiInfo(InRedDotConditions, InTargetIndex));
+    
+    // broadcast.
+    m_OnContentReddot.Broadcast(InRedDotConditions);
+    
+    // save.
+    SaveAsync();
+    
+    return true;
+}
+
+bool UReddotSystem::RemoveContentReddot(const ERedDotConditions& InContentEnum, const int64& InTargetIndex)
+{
+    // RewardReceiving 타입의 레드닷을 제거해줌.
+    if (InContentEnum == ERedDotConditions::None)
+    {
+        UE_LOG(LogTemp, Warning, TEXT("InContentEnum is None"));
+        return false;
+    }
+
+    if (m_ReddotData == nullptr || m_ReddotData->IsValidLowLevel() == false)
+    {
+        UE_LOG(LogTemp, Warning, TEXT("[UReddotSystemData] is null"));
+        return false;
+    }
+
+    m_ReddotData->ContentReddotList.RemoveAll([this, InContentEnum, InTargetIndex](const FReddotNotiInfo& InInfo)
+        {
+            return InContentEnum == InInfo.RedDotConditions
+                && InTargetIndex == InInfo.TargetIndex;
+        });
+
+    // save.
+    SaveAsync();
+
+    return true;
+}
+
+#pragma endregion
+
+// public - Content Reddot.
+//===============================================================================================================================================================================
+// private.
+
+#pragma region Save_Reddot
+void UReddotSystem::SaveAsync()
+{
+    UGameplayStatics::AsyncSaveGameToSlot(m_ReddotData, m_ReddotDataKey, 0);
+}
+
+void UReddotSystem::LoadAsync()
+{
+    FAsyncLoadGameFromSlotDelegate AsyncLoadGameFromSlotDelegate;
+    AsyncLoadGameFromSlotDelegate.BindUFunction(this, TEXT("LoadGameFromSlot"));
+
+    UGameplayStatics::AsyncLoadGameFromSlot(m_ReddotDataKey, 0, AsyncLoadGameFromSlotDelegate);
+}
+
+void UReddotSystem::LoadGameFromSlot(const FString& InUserName, const int32 InUserIndex, USaveGame* InSaveGame)
+{
+    m_ReddotData = Cast<UReddotSystemData>(InSaveGame);
+
+    if (m_ReddotData == nullptr || m_ReddotData->IsValidLowLevel() == false)
+    {
+        // 없으면 새로 만듦.
+        m_ReddotData = Cast<UReddotSystemData>(UGameplayStatics::CreateSaveGameObject(UReddotSystemData::StaticClass()));
+    }
+}
+
+#pragma endregion
---
+++ ReddotSystem.h
@@ -0,0 +1,114 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+#pragma once
+
+#include "CoreMinimal.h"
+
+#include "Object.h"
+#include "GameFramework/SaveGame.h"
+#include "Kismet/GameplayStatics.h"
+
+#include "ReddotSystem.generated.h"
+
+/**
+ *
+ */
+
+UENUM(BlueprintType)
+enum class ERedDotConditions : uint8
+{
+	None,
+	Achievements,
+	Mail, 
+	Exchange, 
+	Inventory, 
+	Friend, 
+	Quest, 
+	Guild,
+	Shop,
+	AccountSkill,
+};
+
+USTRUCT(BlueprintType)
+struct FReddotNotiInfo
+{
+	GENERATED_USTRUCT_BODY()
+
+public:
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite)
+		ERedDotConditions RedDotConditions;
+
+public:
+	FReddotNotiInfo()
+		:RedDotConditions(ERedDotConditions::None),
+	{};
+
+	FReddotNotiInfo(ERedDotConditions InRedDotConditions, int64 InTargetIndex)
+		:RedDotConditions(InRedDotConditions),
+	{};
+};
+
+DECLARE_MULTICAST_DELEGATE_OneParam(FOnContentReddotEvent, const ERedDotConditions&);
+
+UCLASS()
+class UReddotSystem : public UObject
+{
+	GENERATED_BODY()
+
+public:
+	virtual void Initialize() override;
+	virtual void Start() override;
+	virtual void Release() override;
+	//=========================================================================================================
+
+#pragma region Get_Reddot
+public:
+	/**
+	* 해당 컨텐츠에 레드닷 정보가 있는지 확인.
+	* @param ERedDotConditions 레드닷 컨텐츠 타입.
+	*/
+	bool GetReddot(const ERedDotConditions& InRedDotConditions);
+
+	/**
+	* 해당 컨텐츠에 레드닷 정보가 있는지 확인.
+	* @param ERedDotConditions 레드닷 컨텐츠 타입.
+	* @param TArray<int64> 해당 레드닷 타입에 띄울 정보.
+	*/
+	bool GetReddotInfo(const ERedDotConditions& InContentEnum, TArray<int64>& OutReddotList);
+#pragma endregion
+
+#pragma region Set_Reddot
+public:
+	/**
+	* 새롭게 레드닷을 띄워야 하는 컨텐츠.
+	* @param ERedDotConditions 레드닷 컨텐츠 타입.
+	* @param int64 레드닷 조건.
+	*/
+	bool SetReddot(const ERedDotConditions& InRedDotConditions, const int64& InTargetIndex);
+	
+	/**
+	* 레드닷을 더이상 띄우지 않아도 될 때 호출.
+	* @param ERedDotConditions 레드닷 컨텐츠.
+	* @param int64 레드닷 조건.
+	*/
+	bool RemoveContentReddot(const ERedDotConditions& InContentEnum, const int64& InTargetIndex);
+#pragma endregion
+
+	//=========================================================================================================
+
+#pragma region Save_Reddot
+private:
+	void SaveAsync();
+	void LoadAsync();
+
+	UFUNCTION() void LoadGameFromSlot(const FString& InUserName, const int32 InUserIndex, USaveGame* InSaveGame);
+#pragma endregion
+
+public:
+	FOnContentReddotEvent m_OnContentReddot;
+
+private:
+	const FString m_ReddotDataKey = TEXT("_Reddot");
+
+	UPROPERTY()		UReddotSystemData* m_ReddotData = nullptr;
+
+};
